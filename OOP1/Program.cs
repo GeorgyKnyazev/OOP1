9e09008f1107318b003ee331a862413399d09858
Георгий Князев <saint_georgy@yahoo.com>
2023-07-13 11:02:51 +0300

    Добавьте файлы проекта.

---
+++ OOP1/Program.cs
@@ -0,0 +1,39 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Иван" , "Иванов", 45);
+
+            player1.ShowPlayerData();
+
+            Console.ReadKey();
+        }
+
+        class Player
+        {
+            public string Name;
+            public string Surname;
+            public int Age;
+
+            public Player(string name, string surname, int age)
+            {
+                Name = name;
+                Surname = surname;
+                Age = age;
+            }
+
+            public void ShowPlayerData()
+            {
+                Console.WriteLine("Имя " + Name);
+                Console.WriteLine("Фамилия " + Surname);
+                Console.WriteLine("Возвраст " + Age);
+            }
+        }
+    }
+}
